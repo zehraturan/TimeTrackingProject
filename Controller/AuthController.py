a5f3ce76ea2c58a5676c7c13be1b11025ee1ca44
zehraturan <78978788+zehraturan@users.noreply.github.com>
2022-11-07 10:39:12 +0100

    signup email check

--- Controller/AuthController.py
+++ Controller/AuthController.py
@@ -1,9 +1,11 @@
 from Model.User import User
 from DB.DbAccess import DbAccess
+import re
 
 class AuthController :
     dbAccess = DbAccess()
-
+    regex = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b'
+    
     def login(self,email):
         if self.dbAccess.isExistUser(email):
             self.current_user = self.dbAccess.getUser(email)
@@ -14,8 +16,10 @@ class AuthController :
     def signup(self, email, name):
         if self.dbAccess.isExistUser(email):
             return "Already exist"
-        else:
+        elif(re.fullmatch(self.regex, email)):
             user = User(email, name)
             self.dbAccess.saveUser(user)
             return "Success"
+        else:
+            return "Fail"
         
